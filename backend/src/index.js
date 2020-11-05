37e587575ce0099cae4a8ae1ef1aa91418860b6b
Tonybsilva-dev <anoniobsilvaprof@hotmail.com>
2020-11-05 11:43:14 -0300

    Server started!

---
+++ backend/src/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const { uuid, isUuid } = require('uuidv4')
+
+const app = express();
+app.use(express.json())
+
+const projects = []
+
+function logRequests(req, res, next){
+  const { method, url } = req;
+  const logLabel = `[${method.toUpperCase()}] ${url}`
+  console.log(logLabel);
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel);
+}
+app.use(logRequests)
+
+function validateProjectId(req, res, next){
+  const { id } = req.params;
+  if (!isUuid(id)){
+    return res.status(400).json({ error: `❌ This id: '${id}' is a invalid UUID` })
+  }
+  return next()
+}
+app.use('/projects/:id', validateProjectId)
+
+app.get('/projects', (req, res) => {
+
+  const { title } = req.query;
+  const results = title 
+  ? projects.filter(project => project.title.includes(title))
+  : projects;
+
+  return res.json(results)
+})
+
+app.post('/projects', (req, res) => {
+  const { title, owner } = req.body;
+  const project = { id: uuid(), title, owner }
+  projects.push(project)
+  return res.json(project)
+})
+
+app.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, owner } = req.body;
+  const projectIndex = projects.findIndex(project => project.id === id);
+  const project = {
+    id,
+    title,
+    owner
+  };
+
+  if(projectIndex < 0){
+    return res.status(400).json({ error: "❌ Project not found!" })
+  }
+    projects[projectIndex] = project;
+    return res.json({ ok: "✔ Project found!", Project: project })
+
+})
+
+app.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const projectIndex = projects.findIndex(project => project.id === id);
+  if (projectIndex < 0){
+    return res.status(400).json({ error: "❌ Project not found!" })
+  }
+  else{
+    projects.splice(projectIndex, 1)
+    return res.status(204).send(`Project successfully deleted`)
+  }
+})
+
+app.listen(3333, () => {
+  console.log('🚀 Backend started!')
+});
+

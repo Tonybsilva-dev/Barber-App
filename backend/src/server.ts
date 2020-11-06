12f6c21d04228257ac1c36134058993ae9cb85a7
Tonybsilva-dev <anoniobsilvaprof@hotmail.com>
2020-11-06 09:32:35 -0300

    Database created && connected to docker

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import './database'
 
 const app = express();
 app.use(routes);
@@ -7,6 +8,6 @@ app.use(express.json())
 
 
 
-app.listen(3333, () => {
+app.listen(3334, () => {
   console.log('🚀 Server started on port 3333!');
 })

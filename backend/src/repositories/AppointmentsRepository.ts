afcd173adf724599f3ca101edb0373a889277de9
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2020-12-17 10:26:48 -0300

    Appointment repository created

--- backend/src/repositories/AppointmentsRepository.ts
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -1,13 +1,14 @@
-import { isEqual } from 'date-fns'
+import { EntityRepository, Repository } from 'typeorm'
+
 import Appointment from '../models/Appointments';
 
-
-class AppointmentRepository{
-  public findByDate(date: Date): Appointment | null{
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
- );
- return findAppointment || null
+@EntityRepository(Appointment)
+class AppointmentRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date: date }
+    })
+    return findAppointment || null
   }
 }
 

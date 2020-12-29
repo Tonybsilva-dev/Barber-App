14496d9216d6a9cd060e3f0a870fecbd86a2ce22
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2020-12-29 11:33:27 -0300

    server.ts updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,19 +8,25 @@ import './database';
 const app = express();
 app.use(express.json());
 
-// Middleware para log de requisições do frontend.
-// Bom para saber qual rota o usuário está, e que tipo de requisição ela faz.
-// Aqui ela traz o método e a url acessada.
-function logRequests(req: any, res: any, next: any){
-  const { method, url } = req;
+
+// ========== FUNCTIONS ==========
+
+/* Middleware para log de requisições.
+Bom para saber qual rota o usuário está, e que tipo de requisição ela faz.
+Aqui ela traz o método e a url acessada. */
+
+function logRequests(request: any, response: any, next: any){
+  const { method, url } = request;
   const logLabel = `[${method.toUpperCase()}] ${url}`
   console.log(logLabel);
   console.time(logLabel);
   next();
   console.timeEnd(logLabel);
 }
-app.use(logRequests)
 
+// ========== END FUNCTIONS ==========
+
+app.use(logRequests)
 app.use(routes);
 
 const port = 3334

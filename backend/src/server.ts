bda4be85f730bb697d75f4640fd67842bbd0d533
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2020-12-29 11:13:40 -0300

    Bug fixed on middleware added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,20 @@ import './database';
 
 const app = express();
 app.use(express.json());
+
+// Middleware para log de requisições do frontend.
+// Bom para saber qual rota o usuário está, e que tipo de requisição ela faz.
+// Aqui ela traz o método e a url acessada.
+function logRequests(req: any, res: any, next: any){
+  const { method, url } = req;
+  const logLabel = `[${method.toUpperCase()}] ${url}`
+  console.log(logLabel);
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel);
+}
+app.use(logRequests)
+
 app.use(routes);
 
 const port = 3334
